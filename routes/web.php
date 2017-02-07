1fea01495792e3c173b2c6a138db1e0c3b860e2c
Johan <jmarchan@jockeyclub.com.ar>
2017-02-07 15:36:11 -0300

    corrige ruta a  la página de inicio

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 // ******** WS Caballerizas ****************
 Route::get('/caballerizas/', 'StableController@index');
 Route::get('/caballerizas/porId/{id}', 'StableController@show');
@@ -40,12 +42,10 @@ Route::get('/profesionales/porNombre/{nombre}', 'ProfessionalController@porNombr
 
 
 // ********** WS de Sanciones para todos *************
-//Route::get('/sanciones/profesionales/', 'SancionesProfessionalsController@index');
+Route::get('/sanciones/profesionales/', 'SancionesProfessionalsController@index');
 //Route::get('/sanciones/profesionales/porId/{id}', 'SancionesProfessionalsController@porId');
-
 //Route::get('/sanciones/ejemplares/', 'SancionesHorsesController@index');
 //Route::get('/sanciones/ejemplares/porId/{id}', 'SancionesHorsesController@porId');
-
-Route::get('/sanciones/caballerizas/', 'SancionesStablesController@index');
+//Route::get('/sanciones/caballerizas/', 'SancionesStablesController@index');
 //Route::get('/sanciones/caballerizas/porId/{id}', 'SancionesStablesController@porId');
 
